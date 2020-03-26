f77a380a180b4e28697085ceaaf4cf027e5d81a8
unknown <anshori@undiksha.ac.id>
2020-03-26 13:56:01 +0700

    pertama commit

---
+++ resources/views/admin/beranda.blade.php
@@ -0,0 +1,53 @@
+@extends('Layout.dasboard')
+
+@section('content')
+<div class="main-content">
+				<div class="container-fluid">
+					<!-- OVERVIEW -->
+					<div class="panel panel-headline">
+						<div class="panel-heading">
+							<h3 class="panel-title">Menu Utama</h3>
+							<p class="panel-subtitle">Date: Saturday, March 21 2020</p>
+						</div>
+						<div class="panel-body">
+							
+                        <div class="row">
+						<div class="col-md-6">
+							<!-- RECENT PURCHASES -->
+							<div class="panel">
+								<div class="panel-heading">
+									<h3 class="panel-title">IDENTITAS</h3>
+								</div>
+								<div class="panel-body">
+                                    <p>Nama : Anshori Iqbal</p>
+                                    <P>NIM  : 1815051041</P>
+                                    <P>Prodi : Pendidikan Teknik Informatika</P>
+                                </div>
+								
+							</div>
+							<!-- END RECENT PURCHASES -->
+						</div>
+						<div class="col-md-6"> 
+							<!-- MULTI CHARTS -->
+							<div class="panel">
+								<div class="panel-heading">
+									<h3 class="panel-title">Studi Kasus</h3>
+								</div>
+								<div class="panel-body">
+                                    <p>Judul : Sistem Apotek Online</p>
+                                    <p>Penjelasan : Memudahakan dalam pencatatan penjualan dan pembelian obat secara online</p>
+								
+								</div>
+							</div>
+							<!-- END MULTI CHARTS -->
+						</div>
+					</div>
+						</div>
+					</div>
+					<!-- END OVERVIEW -->
+					
+					
+					
+				</div>
+			</div>
+@endsection('content')
